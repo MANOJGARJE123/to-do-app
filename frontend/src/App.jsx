886100ca9cdfabed64ace528d18c15cac6a6ed42
MANOJGARJE123 <manojgarje123@gmail.com>
2025-11-14 14:46:09 +0530

    configures/router and routes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
-function App() {
+import "./App.css";
 
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
   return (
-    <div>Welcome to my To-Do List</div>
-  )
+    <BrowserRouter>
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
